d346dd4d4887055c5b4eb6c274d1af345b134e9b
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-09 21:46:15 +0900

    [추가] dragStart 기능

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
 const carousel = document.querySelector(".carousel");
 
+let isDragStart = false;
+
+const dragStart = () => {
+  isDragStart = true;
+};
+
 const dragging = (e) => {
+  if (!isDragStart) return;
+  // e.preventDefault();
   carousel.scrollLeft = e.pageX;
 };
 
+carousel.addEventListener("mousedown", dragStart);
 carousel.addEventListener("mousemove", dragging);
+// carousel.addEventListener("mouseup", dragStop);
 
 //pageX
-//scrollLeft
+
+//마우스클릭 VS 마우스 다운
+//https://kimgeuntae.tistory.com/16
