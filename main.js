b8dcfbb17c7e4cc021885666d2ed0ad1db15a344
rlawlsdn263 <rlawlsdn263@naver.com>
2023-01-09 23:43:27 +0900

    [추가] 모바일 환경 고려한 이벤트 리스너

--- main.js
+++ main.js
@@ -56,9 +56,14 @@ const dragStop = () => {
 };
 
 carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("touchstart", dragStart);
+
 carousel.addEventListener("mousemove", dragging);
+carousel.addEventListener("touchmove", dragging);
+
 carousel.addEventListener("mouseup", dragStop);
 carousel.addEventListener("mouseleave", dragStop);
+carousel.addEventListener("touchend", dragStop);
 
 //pageX
 
